f040b5b30feba561d3a99aea762d7aacdd9e7abb
saramgsilva <saramgsilva@gmail.com>
2015-03-22 21:03:23 +0000

    Step 5: Add ShareService

---
+++ 1010ENEI/5. Add ShareService/ENEI.SessionsApp/ENEI.SessionsApp/App.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ENEI.SessionsApp.Views;
+using Xamarin.Forms;
+
+namespace ENEI.SessionsApp
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new NavigationPage(new SessionsView())
+            {
+                BarBackgroundColor = Color.White,
+                BarTextColor = Color.Black
+            };
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ 1010ENEI/5. Add ShareService/ENEI.SessionsApp/ENEI.SessionsApp/Converters/FavoriteImageConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ENEI.SessionsApp.Converters
+{
+    public class FavoriteImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                if (Device.OS == TargetPlatform.WinPhone)
+                {
+                    return "Images/ic_action_heart.png";
+                }
+                if (Device.OS == TargetPlatform.iOS)
+                {
+                    return "http://s20.postimg.org/qng9erqvt/ic_action_heart.png";
+                }
+                return "ic_action_heart.png";
+            }
+            var isFavorite = (bool) value;
+
+            if (isFavorite)
+            {
+                if (Device.OS == TargetPlatform.WinPhone)
+                {
+                    return "Images/ic_action_heart_red.png";
+                }
+                if (Device.OS == TargetPlatform.iOS)
+                {
+                    return "http://s20.postimg.org/jbat988nt/ic_action_heart_red.png";
+                }
+                return "ic_action_heart_red.png";
+            }
+            if (Device.OS == TargetPlatform.WinPhone)
+            {
+                return "Images/ic_action_heart.png";
+            }
+            if (Device.OS == TargetPlatform.iOS)
+            {
+                return "http://s20.postimg.org/qng9erqvt/ic_action_heart.png";
+            }
+            return "ic_action_heart.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
