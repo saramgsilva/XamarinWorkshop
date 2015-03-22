13434e09ba0eaceaf51c5a5395ebec5e0c9ce677
saramgsilva <saramgsilva@gmail.com>
2015-03-22 19:12:47 +0000

    Step 3: Add images & assets and Create the SessionsView.

---
+++ 1010ENEI/3. Create the SessionsView/3.2 Create the Listview/ENEI.SessionsApp/ENEI.SessionsApp/Views/SessionsView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.ObjectModel;
+using ENEI.SessionsApp.Data;
+using ENEI.SessionsApp.Model;
+using Xamarin.Forms;
+
+namespace ENEI.SessionsApp.Views
+{
+    public partial class SessionsView : ContentPage
+    {
+        public SessionsView()
+        {
+            InitializeComponent();
+            Sessions = new ObservableCollection<Session>();
+            BindingContext = this;
+        }
+
+        public ObservableCollection<Session> Sessions { get; set; }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (Sessions.Count == 0)
+            {
+                var sessions = SessionsDataSource.GetSessions();
+                foreach (var session in sessions)
+                {
+                    Sessions.Add(session);
+                }
+            }
+        }
+
+        private void SessionsList_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (SessionsList.SelectedItem == null)
+            {
+                return;
+            }
+
+            SessionsList.SelectedItem = null;
+        }
+
+    }
+}
