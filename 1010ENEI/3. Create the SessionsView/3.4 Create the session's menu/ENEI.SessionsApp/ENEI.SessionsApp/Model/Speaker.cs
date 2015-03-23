70be3bc3d43e4e4b6428834b30e2888f408db88f
saramgsilva <saramgsilva@gmail.com>
2015-03-23 20:52:40 +0000

    Add improvements in Speaker class.

--- 1010ENEI/3. Create the SessionsView/3.4 Create the session's menu/ENEI.SessionsApp/ENEI.SessionsApp/Model/Speaker.cs
+++ 1010ENEI/3. Create the SessionsView/3.4 Create the session's menu/ENEI.SessionsApp/ENEI.SessionsApp/Model/Speaker.cs
@@ -5,8 +5,20 @@ namespace ENEI.SessionsApp.Model
 {
     public class Speaker : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string ImageUrl { get; set; }
+        private string _name;
+        private string _imageUrl;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged();}
+        }
+
+        public string ImageUrl
+        {
+            get { return _imageUrl; }
+            set { _imageUrl = value; OnPropertyChanged(); }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
